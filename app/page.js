9b57c87a6f57cc2bc9cb92a1cb9b8b00ab9debf8
hokagecreativelabs <hokagecreativelabs001@gmail.com>
2025-04-15 20:07:35 +0100

    commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import About from "@/components/sections/AboutSection";
-import Announcements from "@/components/sections/AnnouncementSection";
-import UpcomingEvents from "@/components/sections/EventsSection";
+import { useEffect, useState } from 'react';
 import Hero from "@/components/sections/HeroSection";
+import About from "@/components/sections/AboutSection";
+import UpcomingEvents from "@/components/sections/EventsSection";
+import Announcements from "@/components/sections/AnnouncementSection";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Set the client-side flag when the component mounts
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    // Prevent rendering until after the component has mounted
+    return null;
+  }
+
   return (
     <div>
       <Hero />
